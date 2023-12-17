8ca9b565b9b143c310ddbf6b92ba5e4d161d30f2
Xavier Florensa Berenguer <xavier.florensa@soporte.com>
2023-12-17 19:42:22 +0100

    First commit

---
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,20 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace PLC2MQTTClient_HiveMQ_v1_Git
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("PLC2MQTTClient_HiveMQ_v1_Git");
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("00734b27df39a00908c08a25a21bc641"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("PLC2MQTTClient_HiveMQ_v1_Git");
+    }
+}
---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,11 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "PLC2MQTTClient_HiveMQ_v1_Git", Guid = "00734b27df39a00908c08a25a21bc641")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
